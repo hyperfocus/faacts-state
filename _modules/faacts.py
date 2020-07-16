1559eaae5781065e6c164bba277642e0a860ee43
Scott Tully <scott.tully@gmail.com>
2020-07-16 10:13:34 -0400

    Create faacts.py

---
+++ _modules/faacts.py
@@ -0,0 +1,43 @@
+import query from salt.utils.http
+import logging
+
+log = logging.getLogger(__name__)
+
+__virtual_name__ = 'faacts'
+
+def __virtual__():
+  return __virtual_name__
+
+def rest(**kwargs):
+  '''
+  Makes a RESTful request
+  
+  CLI Example::
+
+      salt minion faacts.rest something
+  '''
+  restargs = {}
+  for name in ['path', 'method', 'username', 'password', '', 'binddn','bindpw', 'anonymous']:
+    restargs[name] = _config(name, **kwargs)
+    
+  query(
+    "http://155.178.172.254:8188/cxf/slc/NCRServices?ncr_service=wfs",
+    method="POST",
+	  status=True,
+	  headers=False,
+	  text=False,
+	  username="ncr_test_ext2",
+	  password="4Sk1K8s3q9h7uJr",
+    header_file="/tmp/headers.txt",
+	  data_file="/tmp/data.txt",
+	  text_out="/tmp/data_response.txt",
+	  headers_out="/tmp/headers_response.txt",
+  )
+  
+  
+  log.debug(signs)
+  return_value = {}
+  signs = signs.split(',')
+  for sign in signs:
+    return_value[sign] = _make_request(sign)
+  return return_value
