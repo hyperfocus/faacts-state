eff2c5b099720daae086adc62144387fb93d89e3
Scott Tully <scott.tully@gmail.com>
2020-07-19 18:43:37 -0400

    Update faacts.py

--- _modules/faacts.py
+++ _modules/faacts.py
@@ -29,7 +29,6 @@ def rest(**kwargs):
 	  password="4Sk1K8s3q9h7uJr",
           header_file="/tmp/headers.txt",
 	  data_file="/tmp/data.txt",
-	  backend="requests",
   )
   return ret['headers']
   # ext_out="/tmp/data_response.txt"
