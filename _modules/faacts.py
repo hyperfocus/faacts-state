0da814fbbf2798be0399d3dacd672ab90a59624f
Scott Tully <scott.tully@gmail.com>
2020-07-19 16:27:21 -0400

    Update faacts.py

--- _modules/faacts.py
+++ _modules/faacts.py
@@ -20,17 +20,18 @@ def rest(**kwargs):
   #for name in ['path', 'method', 'username', 'password']:
   #  restargs[name] = _config(name, **kwargs)
     
-  return http.query( "http://155.178.172.254:8188/cxf/slc/NCRServices?ncr_service=wfs",
+  ret = http.query( "http://155.178.172.254:8188/cxf/slc/NCRServices?ncr_service=wfs",
           method="POST",
 	  status=True,
 	  headers=True,
 	  text=True,
-          decode=True,
 	  username="ncr_test_ext2",
 	  password="4Sk1K8s3q9h7uJr",
           header_file="/tmp/headers.txt",
 	  data_file="/tmp/data.txt",
+	  backend="requests",
   )
+  return ret['headers']
   # ext_out="/tmp/data_response.txt"
   # headers_out="/tmp/headers_response.txt"
   
